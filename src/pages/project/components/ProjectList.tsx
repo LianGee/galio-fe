85931ebeeaaaa905a683984580c017d476d684f7
zaoshu <zaoshu@pinduoduo.com>
2020-06-28 16:15:26 +0800

    project search

--- src/pages/project/components/ProjectList.tsx
+++ src/pages/project/components/ProjectList.tsx
@@ -1,168 +1,89 @@
 import React, { Component } from 'react';
-import { Avatar, Card, Descriptions, List, message, Typography } from 'antd';
+import { Avatar, Card, Descriptions, List, Typography } from 'antd';
 import { CiCircleOutlined, DeleteOutlined, FormOutlined, HomeOutlined, RocketOutlined } from '@ant-design/icons';
-import ProjectForm from '@/pages/project/components/ProjectForm';
-import { list, save } from '@/services/project';
 
 interface ProjectListProps {
-
+  data: any;
+  loading: boolean;
+  editProject: any;
 }
 
 interface ProjectListState {
-  visible: boolean;
-  data: any;
-  initialValues: any;
-  title: any;
 }
 
 const { Paragraph } = Typography;
-const initialValues = {
-  namespace: 'default',
-  type: 0,
-  base_image: 'nginx',
-  dockerfile_type: 1,
-  nginx_proxies: [
-    { rule: '^api', server: '' },
-  ],
-  service_type: 0,
-};
 
 class ProjectList extends Component<ProjectListProps, ProjectListState> {
   constructor(props: any) {
     super(props);
-    this.state = {
-      visible: false,
-      data: [],
-      initialValues,
-      title: '新建项目',
-    };
+    this.state = {};
   }
 
   componentDidMount(): void {
-    this.init();
   }
-
-  init = () => {
-    list().then(response => {
-      if (response.status === 0) {
-        this.setState({
-          data: response.data.concat({}),
-          visible: false,
-        });
-      } else {
-        message.error(response.msg);
-      }
-    });
-  };
-
-  addProject = () => {
-    this.setState({
-      visible: true,
-      title: '新建项目',
-      initialValues,
-    });
-  };
-
-  onCancel = () => {
-    this.setState({
-      visible: false,
-    });
-  };
-
-  onOk = (values: any) => {
-    save(values).then(response => {
-      if (response.status === 0) {
-        message.success('保存成功');
-        this.init();
-      } else {
-        message.error(response.msg);
-      }
-    });
-  };
-
-  editProject = (project: any) => {
-    this.setState({ visible: true, initialValues: project, title: '修改项目' });
-  };
 
   render() {
     return <>
       <List
+        loading={this.props.loading}
         grid={{ gutter: 24, column: 3 }}
-        dataSource={this.state.data}
+        dataSource={this.props.data}
         rowKey="id"
         renderItem={(item: any) => (
           <List.Item>
-            {
-              item.id ?
-                <Card
-                  actions={[
-                    <DeleteOutlined/>,
-                    <FormOutlined onClick={() => this.editProject(item)}/>,
-                    <CiCircleOutlined
-                      onClick={() => {
-                        window.location.href = `/build/create?project_id=${item.id}`;
+            <Card
+              actions={[
+                <DeleteOutlined/>,
+                <FormOutlined onClick={() => this.props.editProject(item)}/>,
+                <CiCircleOutlined
+                  onClick={() => {
+                    window.location.href = `/build/create?project_id=${item.id}`;
+                  }}
+                />,
+                <RocketOutlined
+                  onClick={() => {
+                    window.location.href = `/deploy/image?project_id=${item.id}`;
+                  }}
+                />,
+                <HomeOutlined onClick={() => {
+                  window.open(`http://${item.domain}`);
+                }}/>,
+              ]}
+              hoverable
+            >
+              <Card.Meta
+                title={item.name}
+                avatar={
+                  <Avatar src={item.logo} size={60}/>
+                }
+                description={item.description}
+              />
+              <Descriptions column={1} bordered size="small" style={{ marginTop: 20 }}>
+                <Descriptions.Item label="owner">
+                  {item.user_name}
+                </Descriptions.Item>
+                <Descriptions.Item label="端口">
+                  {item.port}
+                </Descriptions.Item>
+                <Descriptions.Item label="类型">
+                  {item.type === 0 ? '前端' : '后端'}
+                </Descriptions.Item>
+                <Descriptions.Item label="内部域名">
+                  <div style={{ width: 160 }}>
+                    <Paragraph
+                      copyable
+                      ellipsis={{
+                        rows: 1,
                       }}
-                    />,
-                    <RocketOutlined
-                      onClick={() => {
-                        window.location.href = `/deploy/image?project_id=${item.id}`;
-                      }}
-                    />,
-                    <HomeOutlined onClick={() => {
-                      window.open(`http://${item.domain}`);
-                    }}/>,
-                  ]}
-                  hoverable
-                >
-                  <Card.Meta
-                    title={item.name}
-                    avatar={
-                      <Avatar src={item.logo} size={60}/>
-                    }
-                    description={item.description}
-                  />
-                  <Descriptions column={1} bordered size="small" style={{ marginTop: 20 }}>
-                    <Descriptions.Item label="owner">
-                      {item.user_name}
-                    </Descriptions.Item>
-                    <Descriptions.Item label="端口">
-                      {item.port}
-                    </Descriptions.Item>
-                    <Descriptions.Item label="类型">
-                      {item.type === 0 ? '前端' : '后端'}
-                    </Descriptions.Item>
-                    <Descriptions.Item label="内部域名">
-                      <div style={{ width: 160 }}>
-                        <Paragraph
-                          copyable
-                          ellipsis={{
-                            rows: 1,
-                          }}
-                        >
-                          {item.service_domain}
-                        </Paragraph>
-                      </div>
-                    </Descriptions.Item>
-                  </Descriptions>
-                </Card>
-                :
-                <Card
-                  hoverable
-                  onClick={this.addProject}
-                >
-                  新建项目
-                </Card>
-            }
-
+                    >
+                      {item.service_domain}
+                    </Paragraph>
+                  </div>
+                </Descriptions.Item>
+              </Descriptions>
+            </Card>
           </List.Item>
         )}
-      />
-      <ProjectForm
-        visible={this.state.visible}
-        onCancel={this.onCancel}
-        onOk={this.onOk}
-        initialValues={this.state.initialValues}
-        title={this.state.title}
       />
     </>;
   }
--- src/pages/project/manager/index.tsx
+++ src/pages/project/manager/index.tsx
@@ -1,13 +1,127 @@
+import * as React from 'react';
 import { Component } from 'react';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
-import * as React from 'react';
 import ProjectList from '@/pages/project/components/ProjectList';
+import { Button, Col, Input, message, Row } from 'antd';
+import { PlusCircleOutlined } from '@ant-design/icons/lib';
+import { list, save } from '@/services/project';
+import ProjectForm from '@/pages/project/components/ProjectForm';
 
-class Project extends Component {
+
+interface ProjectProps {
+
+}
+
+interface ProjectState {
+  data: any;
+  loading: boolean;
+  visible: boolean;
+  initialValues: {};
+  title: string;
+}
+
+const { Search } = Input;
+const initialValues = {
+  namespace: 'default',
+  type: 0,
+  base_image: 'nginx',
+  dockerfile_type: 1,
+  nginx_proxies: [
+    { rule: '^api', server: '' },
+  ],
+  service_type: 0,
+};
+
+class Project extends Component<ProjectProps, ProjectState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      data: [],
+      loading: false,
+      visible: false,
+      initialValues,
+      title: '新建项目',
+    };
+  }
+
+  componentDidMount(): void {
+    this.init();
+  }
+
+  init = () => {
+    this.setState({ loading: true });
+    list().then(response => {
+      if (response.status === 0) {
+        this.setState({
+          data: response.data,
+          loading: false,
+        });
+      } else {
+        message.error(response.msg);
+      }
+    });
+  };
+
+  onOk = (values: any) => {
+    save(values).then(response => {
+      if (response.status === 0) {
+        message.success('保存成功');
+      } else {
+        message.error(response.msg);
+      }
+    });
+  };
+
+  editProject = (project: any) => {
+    this.setState({ visible: true, initialValues: project, title: '修改项目' });
+  };
+
+  addProject = () => {
+    this.setState({
+      visible: true,
+      title: '新建项目',
+      initialValues,
+    });
+  };
+
+  onSearch = (value: string) => {
+    const { data } = this.state;
+    const d = data.filter((item: any) => item.name.indexOf(value) >= 0);
+    this.setState({
+      data: d,
+    });
+  };
+
   render() {
     return <PageHeaderWrapper>
-      <ProjectList/>
-    </PageHeaderWrapper>
+      <Row style={{ marginBottom: 20 }}>
+        <Col span={9} offset={14}>
+          <Search
+            enterButton="搜索"
+            onSearch={this.onSearch}
+          />
+        </Col>
+        <Col span={1}>
+          <Button
+            icon={<PlusCircleOutlined/>}
+            type="primary"
+            onClick={this.addProject}
+          />
+        </Col>
+      </Row>
+      <ProjectList
+        data={this.state.data}
+        loading={this.state.loading}
+        editProject={this.editProject}
+      />
+      <ProjectForm
+        visible={this.state.visible}
+        onCancel={() => this.setState({ visible: false })}
+        onOk={this.onOk}
+        initialValues={this.state.initialValues}
+        title={this.state.title}
+      />
+    </PageHeaderWrapper>;
   }
 }
 
